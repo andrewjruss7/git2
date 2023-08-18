7d0eb62004b0136f53174736e55676310731ed73
Sebastian Vengoechea <sebastian.vengoechea2605@gmail.com>
2023-08-17 20:57:47 -0500

    Comentario-sebastian

--- main.js
+++ main.js
@@ -3,3 +3,4 @@ alert('Hello World');
 // Hello, I'm Jilson Medina.Student of the QA analyst course.
 // Hello Jislon, this comment I write from my pc but I'm use your branch. It's great, right?
 // Profe Andrew, usted es un crack.
+// Hello Jilson, have a nice day!
