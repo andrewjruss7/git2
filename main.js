0736ea7e018fde702205d6829e9408ed1e8afb3c
andrew.ruz@tangelolatam.com <andrew.ruz@tangelolatam.com>
2023-08-17 20:19:28 -0500

    Ok

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 alert('Hello World');
 // hello mam, I know program.
 // Hello, I'm Jilson Medina.Student of the QA analyst course.
+// Hello Jislon, this comment I write from my pc but I'm use your branch. It's great, right?
