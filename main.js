b6c24715a7cb0e62defd2da74e79615572ad82d6
majandra21 <mariaguevararuiz21@hotmail.com>
2023-08-18 00:11:44 -0500

    pregunta

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ alert('Hello World');
 // ahora soy estoy escribiendo un comentario en tu rama. :)
 // tengo una duda sobre clonar un repositorio.
 // Cual es la duda?
+//bueno, ahora mi pregunta. Como sabes tuve que cambiarme de ubuntu o windom y todos
+// mi proyectos de visual estaba guardados en ubunto, asi que subi mis repositorios en github y 
+//las clone en windom pero cuando intento correrlas en cypress aparece un erro , 
+//tengo que instalar el cypress o algo? 
